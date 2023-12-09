6345d3a89a2a34cc41908cbfe75f6c753401160d
12ruheenkhan <112749703+12ruheenkhan@users.noreply.github.com>
2023-12-09 18:35:41 +0530

    blog section complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,17 @@ const initSlider = () => {
 }
 
 window.addEventListener("load", initSlider);
+
+// var slider_wrapper = document.querySelector('.slider-wrapper');
+// let testimonials_list = slider_wrapper.getElementsByClassName('testimonials-list');
+
+
+
+// function prev() {
+//     slider_wrapper.prepend(testimonials_list[testimonials_list-1]);
+// }
+
+// setInterval (
+//     function next() {
+//         slider_wrapper.append(testimonials_list[0]);
+//     },1000)
